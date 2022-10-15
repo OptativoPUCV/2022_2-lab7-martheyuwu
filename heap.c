2db223f5a438ecb4d89f1494becd02ba7b9f8974
Replit user <>
2022-10-15 19:41:40 +0000

    new try

--- heap.c
+++ heap.c
@@ -32,6 +32,8 @@ void heap_push(Heap* pq, void* data, int priority){
     pq->size++;
     int pos=pq->size-1;
     while (1){
+      if (pq->size==pq->capacity)
+        pq->heapArray=realloc(pq->heapArray,pq->capacity+1);
       if (pq->heapArray[(pos-1)/2].priority<pq->heapArray[pos].priority){
         aux[0]=pq->heapArray[(pos-1)/2];
         pq->heapArray[(pos-1)/2]=pq->heapArray[pos];
