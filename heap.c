81540d8fc34cf0a9a2ed988ee6712580c95c120e
Replit user <>
2022-10-16 00:33:50 +0000

    new try

--- heap.c
+++ heap.c
@@ -49,8 +49,27 @@ void heap_push(Heap* pq, void* data, int priority){
 
 
 void heap_pop(Heap* pq){
+   heapElem *aux=(heapElem*)malloc(sizeof(heapElem));
+   int pos=0;
    pq->heapArray[0]=pq->heapArray[pq->size-1];
    pq->size--;
+   while (1){
+     if (pq->heapArray[pos].priority<pq->heapArray[2*pos+1].priority || pq->heapArray[pos].priority<pq->heapArray[2*pos+2].priority){
+       aux[0]=pq->heapArray[pos];
+       if (pq->heapArray[2*pos+1].priority>pq->heapArray[2*pos+2]){
+         pq->heapArray[pos]=pq->heapArray[2*pos+1];
+         pq->heapArray[2*pos+1]=aux[0];
+         pos=2*pos+1;
+       }
+       else if (pq->heapArray[2*pos+1].priority<pq->heapArray[2*pos+2]){
+         pq->heapArray[pos]=pq->heapArray[2*pos+2];
+         pq->heapArray[2*pos+2]=aux[0];
+         pos=2*pos+2;
+       }
+       else
+         break;
+     }
+   }
 }
 
 Heap* createHeap(){
