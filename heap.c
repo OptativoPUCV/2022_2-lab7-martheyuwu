93687139c101ec8a1178f1e54f99439bfc25bdfb
Replit user <>
2022-10-16 23:51:55 +0000

    new try

--- heap.c
+++ heap.c
@@ -55,15 +55,22 @@ void heap_pop(Heap* pq){
    pq->size--;
   aux[0]=pq->heapArray[pos];
   if (pq->size>1){
-     if (pq->heapArray[2*pos+1].priority>pq->heapArray[2*pos+2].priority){           pq->heapArray[pos]=pq->heapArray[2*pos+1];
-           pq->heapArray[2*pos+1]=aux[0];
-           pos=2*pos+1;       
+    while (1){
+      if (pq->heapArray[2*pos+1].priority>pq->heapArray[pos].priority || pq->heapArray[2*pos+2].priority>pq->heapArray[pos].priority){
+       if (pq->heapArray[2*pos+1].priority>pq->heapArray[2*pos+2].priority){
+             pq->heapArray[pos]=pq->heapArray[2*pos+1];
+             pq->heapArray[2*pos+1]=aux[0];
+             pos=2*pos+1;       
+        }
+        else if (pq->heapArray[2*pos+1].priority<pq->heapArray[2*pos+2].priority){
+                 pq->heapArray[pos]=pq->heapArray[2*pos+2];
+                 pq->heapArray[2*pos+2]=aux[0];
+                 pos=2*pos+2;
+        }
       }
-      else if (pq->heapArray[2*pos+1].priority<pq->heapArray[2*pos+2].priority){
-               pq->heapArray[pos]=pq->heapArray[2*pos+2];
-               pq->heapArray[2*pos+2]=aux[0];
-               pos=2*pos+2;
-      }
+      else
+        break;
+    }
   }
 }
 
